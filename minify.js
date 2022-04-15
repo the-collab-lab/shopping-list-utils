2ed966af54014cbdfec65db18a44a5603c4ad5cd
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-04-15 15:28:05 -0700

    chore: add note

--- minify.js
+++ minify.js
@@ -1,3 +1,13 @@
+/**
+ * Minify our files after the module is built.
+ *
+ * NOTE: This could probably be more efficient. We can get rid of
+ * the *sync methods from FS, and other optimizations as well.
+ *
+ * Recursively calling getAllFiles might be slow
+ * but that probbably doesn't matter for a file tree this shallow.
+ */
+
 const { minify } = require("terser");
 const fs = require("fs");
 const path = require("path");

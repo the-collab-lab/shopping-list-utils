372cb8f13b9aae289749f40f18bb86999fae2f97
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-04-15 11:59:35 -0700

    feat: remove fallback method for random numbers

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -3,34 +3,26 @@ import { words } from "./words";
 // the below is heavily based on https://andrew.hedges.name/experiments/diceware/
 
 // from: https://www.rempe.us/diceware/
-const secureRandom = (count: number) => {
-  const cryptoObj = window.crypto || window.msCrypto;
+const getRandomInt = (min: number, max: number) => {
+  const crypto = window.crypto;
   const rand = new Uint32Array(1);
-  const skip = 0x7fffffff - (0x7fffffff % count);
+  const skip = 0x7fffffff - (0x7fffffff % max);
   let result: number;
 
-  if (((count - 1) & count) === 0) {
-    cryptoObj.getRandomValues(rand);
-    return rand[0] & (count - 1);
+  if (((max - 1) & max) === 0) {
+    crypto.getRandomValues(rand);
+    return rand[0] & (max - 1);
   }
 
   do {
-    cryptoObj.getRandomValues(rand);
+    crypto.getRandomValues(rand);
     result = rand[0] & 0x7fffffff;
   } while (result >= skip);
 
-  return result % count;
+  return (result % max) + min;
 };
 
 // based on: http://stackoverflow.com/a/1527820/11577
-const getRandomInt = (min: number, max: number) => {
-  if (window.crypto || window.msCrypto) {
-    return secureRandom(max) + min;
-  } else {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-};
-
 export const getToken = () => {
   let phrase: string[] = [];
 
